7f063a96ffe571498419c9823e5243b1e413ae16
CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
2023-11-13 16:55:46 -0700

    TouchScript Test
    Got tap and swipe working using the TouchScript SDK

---
+++ CodexMaya/Assets/Scripts/CubeTap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TouchScript.Gestures;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class CubeTap : MonoBehaviour
+{
+    public float Force = 5f;
+
+    private TapGesture tap;
+    private Rigidbody rb;
+    private Camera activeCamera;
+
+    private void OnEnable()
+    {
+        activeCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody>();
+        tap = GetComponent<TapGesture>();
+        tap.Tapped += tappedHandler;
+    }
+
+    private void tappedHandler(object sender, System.EventArgs e)
+    {
+        var ray = activeCamera.ScreenPointToRay(tap.ScreenPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.transform == transform)
+        {
+            rb.AddForceAtPosition(ray.direction * Force, hit.point, ForceMode.Impulse);
+        }
+    }
+}
