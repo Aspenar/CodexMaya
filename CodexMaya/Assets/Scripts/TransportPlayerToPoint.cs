2aeac843788d64f72c1919c9661cff58af934b3e
CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
2024-02-13 13:14:33 -0700

    Added Screen fade as well as more to scene 1

--- CodexMaya/Assets/Scripts/TransportPlayerToPoint.cs
+++ CodexMaya/Assets/Scripts/TransportPlayerToPoint.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class TransportPlayerToPoint : MonoBehaviour
+public class TransportPlayerToPoint : NetworkBehaviour
 {
     private GameObject player;
+    private OVRScreenFade fade;
 
     private void Awake()
     {
         player = GameObject.Find("Host");
+        fade = player.GetComponent<OVRScreenFade>();
     }
 
     public void MovePlayer()
     {
+        StartCoroutine(StartFade());
+        
+    }
+
+    IEnumerator StartFade()
+    {
+        fade.FadeOut();
+        yield return new WaitForSeconds(1);
         player.transform.position = gameObject.transform.position;
         player.transform.rotation = gameObject.transform.rotation;
+        fade.FadeIn();
     }
 }
