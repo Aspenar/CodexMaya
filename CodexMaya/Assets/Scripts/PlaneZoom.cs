e78353963ab01bac31064a659d664bb0575e4079
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2024-03-27 19:37:00 -0600

    Squashed commit of the following:
    commit b18f40736e2a682fab25c5d1fa6e107b2caddf9e
    Author: Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
    Date:   Wed Mar 27 19:35:52 2024 -0600
    
        added greyout
    
    commit f8d8d428f46c4bc082af16039cd32bbe4473fe1e
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Wed Mar 27 19:25:44 2024 -0600
    
        Anim
    
    commit 0ed776c13cb62b091e51eccbb4d21ca09f899e41
    Author: CarsonCrawford37 <carsoncrawford@u.boisestate.edu>
    Date:   Mon Mar 4 12:40:31 2024 -0700
    
        Workin
    
    commit 59e370f8d8726cff27f9af1dd094b80a413bf48c
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Sat Mar 2 13:34:37 2024 -0700
    
        Working on Night Codex Pages
    
    commit bba1099a02a5095bd5e7a329cfd1606e49c3f8ad
    Merge: 5a689cf 540ab69
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Fri Mar 1 15:34:23 2024 -0700
    
        Merge branch 'Nick' into Carson
    
    commit 5a689cf030b01ad95a4564ab8d802cab9ef75564
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Fri Mar 1 15:27:36 2024 -0700
    
        Uploaded Codex Pages
    
    commit 540ab69d04af66de1c7319e5096198ba1709a844
    Author: LuckyLotspeich <nicklotspeich@u.boisestate.edu>
    Date:   Tue Feb 27 18:47:27 2024 -0700
    
        Spelled Environment Wrong...
    
        Changed the name of the scene and replaced the scene change variable in the main menu
    
    commit 51b4ded6c181712b8f4e3d9fc394720f0582fa66
    Author: LuckyLotspeich <nicklotspeich@u.boisestate.edu>
    Date:   Tue Feb 27 18:28:13 2024 -0700
    
        Change to Night Scene and Adjust Temple Location
    
    commit f7038032cd4cacd55784cf55cf54d0b2b0a29f1b
    Author: LuckyLotspeich <nicklotspeich@u.boisestate.edu>
    Date:   Mon Feb 26 12:28:02 2024 -0700
    
        Added IntroEnvirornment Scene
    
        Created a scene to help illustrate the opening we may want for our project.
    
    commit 4d4574a9b9e7f451849123a259968cefce6cd3e4
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Fri Feb 23 11:35:09 2024 -0700
    
        New Grey Box
    
    commit 53e9f9ee462d266d754ddd18207058da601d31ef
    Author: CarsonCrawford37 <carsoncrawford@u.boisestate.edu>
    Date:   Thu Feb 22 12:17:02 2024 -0700
    
        Working on simple object interactions for pages
    
    commit 6bf8cf84528c2f1631fae9f1e10f1321496a5d90
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Wed Feb 21 11:07:44 2024 -0700
    
        Still working on the greybox...
    
    commit 001d89f432840d995728ada8efed1325794e9543
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Wed Feb 21 10:36:48 2024 -0700
    
        working on greybox
    
    commit a58c6db6e4e1f1d61d03f2a0ab4b07a61d5a9817
    Author: CarsonCrawford37 <carsoncrawford@u.boisestate.edu>
    Date:   Tue Feb 20 17:37:54 2024 -0700
    
        Image Screen Fit
    
    commit 431bbb16ea30937d315c13a763deeb32ffc71165
    Author: CarsonCrawford37 <90733673+CarsonCrawford37@users.noreply.github.com>
    Date:   Sun Feb 18 23:03:55 2024 -0700
    
        New points in temple
    
        When the players go to temple they are now placed in more positions closer to the screen.

--- CodexMaya/Assets/Scripts/PlaneZoom.cs
+++ CodexMaya/Assets/Scripts/PlaneZoom.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaneZoom : MonoBehaviour
 {
+    private GameObject greyOut;
     private bool clicked = false;
     private bool isEnlarged = false;
 
     private Animator anim;
+<<<<<<< Updated upstream
+/*    private Renderer rend;
+    public Material mat1;
+    public Material mat2;
+*/
+=======
 
+>>>>>>> Stashed changes
     private void Awake()
     {
+        GameObject greyOut = GameObject.FindGameObjectWithTag("greyOut");
         anim = GetComponent<Animator>();
+<<<<<<< Updated upstream
+/*        rend = GetComponent<Renderer>();
+        mat1 = rend.material;
+*/    }
+=======
     }
+>>>>>>> Stashed changes
 
     private void Update()
     {
@@ -21,16 +37,31 @@ public class PlaneZoom : MonoBehaviour
         {
             if (!isEnlarged)
             {
+<<<<<<< Updated upstream
+                anim.Play(enlargeAnim);
+/*                rend.material = mat2;
+*/                isEnlarged = true;
+=======
                 anim.Play("Enlarge2");
                 isEnlarged = true;
+                greyOut.SetActive(true);
+                
+>>>>>>> Stashed changes
             }
         }
         else if (!clicked)
         {
             if (isEnlarged)
             {
+<<<<<<< Updated upstream
+                anim.Play(minimizeAnim);
+/*                rend.material = mat1;
+*/                isEnlarged = false;
+=======
                 anim.Play("Minimize");
                 isEnlarged = false;
+                greyOut.SetActive(false);
+>>>>>>> Stashed changes
             }
         }
     }
