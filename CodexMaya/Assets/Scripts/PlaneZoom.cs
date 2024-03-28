3b7b5878c4a9bf99f71eda00164c3e4002e47f2f
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2024-03-27 19:41:41 -0600

    Update PlaneZoom.cs

--- CodexMaya/Assets/Scripts/PlaneZoom.cs
+++ CodexMaya/Assets/Scripts/PlaneZoom.cs
@@ -11,25 +11,11 @@ public class PlaneZoom : MonoBehaviour
     private bool isEnlarged = false;
 
     private Animator anim;
-<<<<<<< Updated upstream
-/*    private Renderer rend;
-    public Material mat1;
-    public Material mat2;
-*/
-=======
-
->>>>>>> Stashed changes
     private void Awake()
     {
         GameObject greyOut = GameObject.FindGameObjectWithTag("greyOut");
         anim = GetComponent<Animator>();
-<<<<<<< Updated upstream
-/*        rend = GetComponent<Renderer>();
-        mat1 = rend.material;
-*/    }
-=======
     }
->>>>>>> Stashed changes
 
     private void Update()
     {
@@ -37,31 +23,18 @@ public class PlaneZoom : MonoBehaviour
         {
             if (!isEnlarged)
             {
-<<<<<<< Updated upstream
                 anim.Play(enlargeAnim);
 /*                rend.material = mat2;
 */                isEnlarged = true;
-=======
-                anim.Play("Enlarge2");
-                isEnlarged = true;
-                greyOut.SetActive(true);
-                
->>>>>>> Stashed changes
             }
         }
         else if (!clicked)
         {
             if (isEnlarged)
             {
-<<<<<<< Updated upstream
                 anim.Play(minimizeAnim);
 /*                rend.material = mat1;
 */                isEnlarged = false;
-=======
-                anim.Play("Minimize");
-                isEnlarged = false;
-                greyOut.SetActive(false);
->>>>>>> Stashed changes
             }
         }
     }
